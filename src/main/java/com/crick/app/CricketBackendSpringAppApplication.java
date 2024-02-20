12b0ec9f073f0f21fcf6c004d6c01092ba87b324
Rishav <rishavdadwal1@gmail.com>
2024-02-20 22:36:47 +0530

    Initial commit

---
+++ src/main/java/com/crick/app/CricketBackendSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.crick.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketBackendSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketBackendSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crick/app/CricketBackendSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crick.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketBackendSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
