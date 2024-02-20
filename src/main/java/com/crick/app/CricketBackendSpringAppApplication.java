d9903bcbdf52d6157d1da8466cef5fc35a0781b4
Rishav <rishavdadwal1@gmail.com>
2024-02-20 22:50:44 +0530

    Update CricketBackendSpringAppApplication.java

--- src/main/java/com/crick/app/CricketBackendSpringAppApplication.java
+++ src/main/java/com/crick/app/CricketBackendSpringAppApplication.java
@@ -7,4 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CricketBackendSpringAppApplication {
 
 	public static void main(String[] args) {
+		SpringApplication.run(CricketBackendSpringAppApplication.class, args);
+	}
+
 }
